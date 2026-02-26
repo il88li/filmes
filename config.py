3729272ca9e51ea484094f8f93927542cca38a42
il88li <yl9667383@gmail.com>
2026-02-27 00:21:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 
 TOKEN = "8338583447:AAHC_hc1IR_fSqgPSK4xpnefHi2mcke8cI4"
-ADMIN_ID = 6689435577  # ايدي المدير
-FORCE_CHANNEL = "@iIl337"  # قناة الاشتراك الإجباري الرسمية
+ADMIN_ID = 6689435577
+FORCE_CHANNEL = "@iIl337"
 FORCE_CHANNEL_LINK = "https://t.me/iIl337"
-
-# أسماء الجداول في قاعدة البيانات
+BOT_USERNAME = "FI7O_BOT"  # بدون @
 DB_NAME = "bot_data.db"
+
+# قيم افتراضية
+DEFAULT_INVITE_COUNT = "5"
