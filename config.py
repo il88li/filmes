425193f68278653ef810690900d1b8abb63e312a
il88li <yl9667383@gmail.com>
2026-02-27 04:27:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ BOT_USERNAME = "FI7O_BOT"
 DB_NAME = "bot_data.db"
 
 # القنوات الافتراضية (سيتم تعيينها تلقائيًا إذا لم تكن موجودة)
-DEFAULT_SERIES_CHANNEL = "@FIL2360"
-DEFAULT_MOVIES_CHANNEL = "@FIL360"
-DEFAULT_RECOMMENDATIONS_CHANNEL = "@TOSI7"
+DEFAULT_SERIES_CHANNEL = "-1003601037185"          # يمكنك تغييره لاحقاً من لوحة التحكم
+DEFAULT_MOVIES_CHANNEL = "-1003886823374"           # يمكنك تغييره لاحقاً
+DEFAULT_RECOMMENDATIONS_CHANNEL = "-1003091756917"   # يمكنك تغييره لاحقاً
+
+# قناة النسخ الاحتياطي (خاصة بالبوت، يجب أن يكون البوت مشرفاً فيها)
+BACKUP_CHANNEL_ID = "-1003844037351"   # ⚠️ غيّر هذا إلى معرف قناتك الخاصة
